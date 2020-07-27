ca16231082acc7642206524c66d5f4afb864e1f6
Jhon <jhonvilcarana@upeu.edu.pe>
2020-07-27 15:51:12 -0500

    first commit

---
+++ src/test/java/pe/edu/upeu/springMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.springMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
